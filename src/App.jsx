c20a7dd81060b574f48d2065d5dea509e0c7abed
laxusrock <133290222+laxusrock@users.noreply.github.com>
2025-01-25 18:06:47 +0100

    crear función handleclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 import "./App.css";
 
-function Square() {
-  const [value, setValue] = useState(null)
+function Square({ value, onSquareClick }) {
+  return ( 
+  <button
+  className="square"
+  onClick={onSquareClick}
+  > 
+  {value}
+  </button>
+)};
 
-  function handleClick() {
-    setValue('X');
-  }
-
-  return (
-    <button
-    className="square"
-    onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
-}
+Square.propTypes = {
+  value: PropTypes.string.isRequired,
+  onSquareClick: PropTypes.func.isRequired,
+};
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+  const newSquares = squares.slice();
+  newSquares[0] = 'X';
+  setSquares(newSquares);
+  }
+  
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
-
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
-
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
